47b7f77c4f97fbc3207fdc90fca245425f07bde9
Caio de Figueiredo <caco.joi@gmail.com>
2019-02-28 15:09:21 -0300

    Primeiro projeto Mongo Curso Java

---
+++ src/main/java/com/curso/mongo/CursoMongoApplication.java
@@ -0,0 +1,13 @@
+package com.curso.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMongoApplication.class, args);
+	}
+
+}
