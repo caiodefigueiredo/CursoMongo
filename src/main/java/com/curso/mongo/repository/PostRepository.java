828411497d7d78ae1f0e323ef892133bdf8a4e2b
Caio de Figueiredo <caco.joi@gmail.com>
2019-03-01 17:59:28 -0300

    Post incluido

---
+++ src/main/java/com/curso/mongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.curso.mongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.curso.mongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+	
+	
+}
